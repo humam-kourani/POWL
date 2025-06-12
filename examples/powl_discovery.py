f61bf6247583d802e0b86c840d3ca399734ad2b7
humam.kourani <humam.kourani@fit.fraunhofer.de>
2025-06-12 19:35:24 +0200

    POWL miner with POWL 2.0 support

---
+++ examples/powl_discovery.py
@@ -0,0 +1,27 @@
+import pm4py
+import powl
+
+def execute_script():
+
+    # Read event log (csv, xes, or xes.gz)
+    log  = powl.import_event_log(r"../examples/running-example.csv")
+
+    # Discover POWL 2.0 model
+    model = powl.discover(log, dfg_frequency_filtering_threshold=0.0)
+
+    # View the discovered model
+    powl.view(model)
+
+    # Export visualization
+    powl.save_visualization_net(model, file_path=r"C:\Users\kourani\Downloads\test_save.svg", use_frequency_tags=False)
+
+    # Convert into a PM4Py Petri net
+    petri_net, initial_marking, final_marking = powl.convert_to_petri_net(model)
+    pm4py.view_petri_net(petri_net, initial_marking, final_marking, format="SVG")
+
+    # Convert into a PM4Py BPMN model
+    bpmn = powl.convert_to_bpmn(model)
+    pm4py.view_bpmn(bpmn, format="SVG")
+
+if __name__ == "__main__":
+    execute_script()
---
+++ powl/__init__.py
@@ -0,0 +1,1 @@
+from powl.main import import_event_log, discover, view, view_net, save_visualization, save_visualization_net, convert_to_petri_net, convert_to_bpmn
