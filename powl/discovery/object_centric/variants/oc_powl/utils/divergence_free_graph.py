5ca1c8507909fc95483b3ad0ea05b58edc875f2a
humam.kourani <humam.kourani@fit.fraunhofer.de>
2025-08-12 20:34:00 +0200

    bug fix in df2 miner: added missing start and end activities

--- powl/discovery/object_centric/variants/oc_powl/utils/divergence_free_graph.py
+++ powl/discovery/object_centric/variants/oc_powl/utils/divergence_free_graph.py
@@ -4,9 +4,11 @@ import pm4py
 
 def get_divergence_free_graph(relations, div, rel):
 
-    global_dfg = {(a,b):0 for a in rel.keys() for b in rel.keys()}
-    global_start = {a:0 for a in rel.keys()}
-    global_end = {a:0 for a in rel.keys()}
+    alphabet = rel.keys()
+
+    global_dfg = {(a,b):0 for a in alphabet for b in alphabet}
+    global_start = {a:0 for a in alphabet}
+    global_end = {a:0 for a in alphabet}
 
     non_starts = []
     non_ends = []
@@ -30,11 +32,19 @@ def get_divergence_free_graph(relations, div, rel):
             if value:
                 global_end[key] += value
 
-        non_starts += [a for a in rel.keys() if not local_start.get(a,0) and ot in rel[a]]
-        non_ends += [a for a in rel.keys() if not local_end.get(a,0) and ot in rel[a]]
+        non_starts += [a for a in alphabet if not local_start.get(a,0) and ot in rel[a]]
+        non_ends += [a for a in alphabet if not local_end.get(a,0) and ot in rel[a]]
 
     global_dfg = {key:value for key,value in global_dfg.items() if value}
     global_start = {key:value for key,value in global_start.items() if value and key not in non_starts}
     global_end = {key:value for key,value in global_end.items() if value and key not in non_ends}
+
+    for node in alphabet:
+        pre = {a for (a, b) in global_dfg if b == node}
+        if len(pre) == 0:
+            global_start[node] = 1
+        post = {b for (a, b) in global_dfg if a == node}
+        if len(post) == 0:
+            global_end[node] = 1
 
     return pm4py.objects.dfg.obj.DFG(global_dfg,global_start,global_end)
