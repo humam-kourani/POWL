ba83b4e53f10026169e3afb180e37709208db6fd
humam.kourani <humam.kourani@fit.fraunhofer.de>
2025-08-13 17:53:57 +0200

    bug fix in df2 miner: connect isolated nodes to start and end

--- powl/discovery/object_centric/variants/oc_powl/utils/divergence_free_graph.py
+++ powl/discovery/object_centric/variants/oc_powl/utils/divergence_free_graph.py
@@ -1,6 +1,10 @@
 # this file is copied from https://github.com/Nik314/DF2-Miner
+from collections import Counter
 
 import pm4py
+
+from powl.objects.utils.relation import get_transitive_closure_from_counter
+
 
 def get_divergence_free_graph(relations, div, rel):
 
@@ -39,12 +43,29 @@ def get_divergence_free_graph(relations, div, rel):
     global_start = {key:value for key,value in global_start.items() if value and key not in non_starts}
     global_end = {key:value for key,value in global_end.items() if value and key not in non_ends}
 
+    closure = get_transitive_closure_from_counter(Counter(global_dfg))
+
     for node in alphabet:
-        pre = {a for (a, b) in global_dfg if b == node}
-        if len(pre) == 0:
-            global_start[node] = 1
-        post = {b for (a, b) in global_dfg if a == node}
-        if len(post) == 0:
-            global_end[node] = 1
+        if node not in closure.keys():
+            closure[node] = set()
+
+    new_starts = set()
+    new_ends = set()
+
+    for node in alphabet:
+        if node not in global_start.keys():
+            reachable_from_start = any(node in closure[start] for start in global_start.keys())
+            if not reachable_from_start:
+                new_starts.add(node)
+        if node not in global_end.keys():
+            reachable_from_end = any(end in closure[node] for end in global_end.keys())
+            if not reachable_from_end:
+                new_ends.add(node)
+
+    for node in new_starts:
+        global_start[node] = 1
+
+    for node in new_ends:
+        global_end[node] = 1
 
     return pm4py.objects.dfg.obj.DFG(global_dfg,global_start,global_end)
