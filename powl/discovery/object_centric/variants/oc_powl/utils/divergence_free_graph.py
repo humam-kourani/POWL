0978b232144e6685e1bf34cb5c0fe326cd995bd9
humam.kourani <humam.kourani@fit.fraunhofer.de>
2025-09-24 12:29:20 +0200

    extended the DFMiner to additionally construct divergent activity partitions per object type

--- powl/discovery/object_centric/variants/oc_powl/utils/divergence_free_graph.py
+++ powl/discovery/object_centric/variants/oc_powl/utils/divergence_free_graph.py
@@ -1,9 +1,38 @@
-# this file is copied from https://github.com/Nik314/DF2-Miner
+# this file was initially copied from https://github.com/Nik314/DF2-Miner
+
 from collections import Counter
-
 import pm4py
-
 from powl.objects.utils.relation import get_transitive_closure_from_counter
+from pm4py.objects.dfg.obj import DFG
+
+
+def _partitions_from_edges(nodes, edges):
+    edges = [tuple(edge) for edge in edges]
+    parts = [{a} for a in nodes]
+
+    def find_group(a):
+        for g in parts:
+            if a in g:
+                return g
+        return None
+
+    for u, v in edges:
+        gu = find_group(u)
+        gv = find_group(v)
+        assert gu is not None and gv is not None
+        if gu is gv:
+            continue
+        gu |= gv
+        parts.remove(gv)
+
+    covered = set().union(*parts) if parts else set()
+    nodes_set = set(nodes)
+    assert covered == nodes_set, "Invalid partition: missing/extra nodes!"
+    assert all(len(g) > 0 for g in parts), "Invalid partition: empty group encountered!"
+    assert sum(len(g) for g in parts) == len(covered), "Invalid partition: overlapping groups!"
+
+    return parts
+
 
 
 def get_divergence_free_graph(relations, div, rel):
@@ -17,6 +46,8 @@ def get_divergence_free_graph(relations, div, rel):
     non_starts = []
     non_ends = []
 
+    divergent_partitions_by_ot = {}
+
 
     for ot in relations["ocel:type"].unique():
 
@@ -24,9 +55,18 @@ def get_divergence_free_graph(relations, div, rel):
         local_dfg, local_start, local_end = pm4py.discover_dfg(sub_log,"ocel:activity",
                                                 "ocel:timestamp","ocel:oid")
 
-        for key,value in local_dfg.items():
-            if value and not (ot in div[key[0]] & div[key[1]]):
-                global_dfg[key] += value
+        filtered_edges = set()
+
+        for key, value in local_dfg.items():
+            if not value:
+                continue
+            source = key[0]
+            target = key[1]
+            if (ot in div[source]) and (ot in div[target]):
+                if source != target:
+                        filtered_edges.add(frozenset((source, target)))
+            else:
+                global_dfg[(source, target)] += value
 
         for key,value in local_start.items():
             if value:
@@ -38,6 +78,9 @@ def get_divergence_free_graph(relations, div, rel):
 
         non_starts += [a for a in alphabet if not local_start.get(a,0) and ot in rel[a]]
         non_ends += [a for a in alphabet if not local_end.get(a,0) and ot in rel[a]]
+
+        divergent_acts_for_ot = {a for a in alphabet if ot in div[a]}
+        divergent_partitions_by_ot[ot] = _partitions_from_edges(divergent_acts_for_ot, filtered_edges)
 
     global_dfg = {key:value for key,value in global_dfg.items() if value}
     global_start = {key:value for key,value in global_start.items() if value and key not in non_starts}
@@ -68,4 +111,4 @@ def get_divergence_free_graph(relations, div, rel):
     for node in new_ends:
         global_end[node] = 1
 
-    return pm4py.objects.dfg.obj.DFG(global_dfg,global_start,global_end)
+    return DFG(global_dfg,global_start,global_end), divergent_partitions_by_ot
