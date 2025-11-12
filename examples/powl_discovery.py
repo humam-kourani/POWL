36fab816060350cd804f80811a8997e2a282d401
humam-kourani <75330540+humam-kourani@users.noreply.github.com>
2025-11-12 17:15:48 +0100

    Update powl_discovery.py
    removed resources

--- examples/powl_discovery.py
+++ examples/powl_discovery.py
@@ -21,21 +21,8 @@ def execute_script():
     pm4py.view_petri_net(petri_net, initial_marking, final_marking, format="SVG")
 
     # Convert into a PM4Py BPMN model
-    activity_to_pool_lane = {
-        'register request' : ("P1", 'Lane1'),
-        'reinitiate request' : ("P2", 'Lane1'),
-        'pay compensation' : ("P1", 'Lane1'),
-        'reject request' : ("P1", 'Lane1'),
-        'examine casually' : ("P2", 'Lane2'),
-        'examine thoroughly' : ("P1", 'Lane2'),
-        'check ticket' : ("P2", 'Lane3'),
-        'decide' : ("P1", 'Lane3'),
-    }
-
-    bpmn_model = to_bpmn_with_resources(activity_to_pool_lane, model)
-    # export it as .bpmn
-    with open(r"./examples/powl_bpmn.bpmn", "w") as f:
-        f.write(bpmn_model)
+    bpmn = powl.convert_to_bpmn(model)
+    pm4py.view_bpmn(bpmn, format="SVG")
 
 if __name__ == "__main__":
     execute_script()
